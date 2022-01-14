e3b97343f22d8e40f2fffaeecd0b8c41c43327bd
Teshome99 <97695543+Teshome99@users.noreply.github.com>
2022-01-13 22:55:01 -0500

    yes done

---
+++ Ec_Test/src/test/classtst.java
@@ -0,0 +1,15 @@
+package test;
+
+public class classtst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" print some thing");
+System.out.println(" print some thing");
+System.out.println(" print some thing");
+System.out.println(" print some thing");
+System.out.println(" print some thing");
+
+	}
+
+}
